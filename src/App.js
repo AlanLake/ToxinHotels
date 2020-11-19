2ed901d498c787e40df1229e572125c00a570774
AlanLake <alan.lake@mail.ru>
2020-11-19 22:39:20 +0300

    added button templates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import "./App.scss";
-import Rating from './components/StarsRating'
+import {
+  ButtonPurple,
+  ButtonPurpleFaded,
+  BlankButton,
+  BlankButtonFaded,
+  WideButton,
+  BorderlessButton,
+  BorderlessButtonFaded,
+} from "./components/Buttons";
 
 function App() {
   return (
     <div className="App">
-      <Rating>
-      </Rating>
+      <ButtonPurple name="click"></ButtonPurple>
+      <ButtonPurpleFaded name="click"></ButtonPurpleFaded>
+      <BlankButton name="click"></BlankButton>
+      <BlankButtonFaded name="click"></BlankButtonFaded>
+      <WideButton name="Перейти к оплате"></WideButton>
+      <BorderlessButton name="Click"></BorderlessButton>
+      <BorderlessButtonFaded name="Click"></BorderlessButtonFaded>
     </div>
   );
 }
-
 
 export default App;
---
+++ src/components/Buttons.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export const ButtonPurple = (props) => {
+  return (
+    <>
+      <button className="button_purple">{props.name}</button>
+    </>
+  );
+};
+
+export const ButtonPurpleFaded = (props) => {
+  return (
+    <>
+      <button className="button_purple_faded">{props.name}</button>
+    </>
+  );
+};
+
+export const BlankButton = (props) => {
+  return (
+    <>
+      <button className="button_blank">{props.name}</button>
+    </>
+  );
+};
+
+export const BlankButtonFaded = (props) => {
+  return (
+    <>
+      <button className="button_blank_faded">{props.name}</button>
+    </>
+  );
+};
+
+export const WideButton = (props) => {
+  return (
+    <>
+      <button className="button_wide">{props.name}</button>
+    </>
+  );
+};
+
+export const BorderlessButton = (props) => {
+  return (
+    <>
+      <button className="button_borderless">{props.name}</button>
+    </>
+  );
+};
+
+export const BorderlessButtonFaded = (props) => {
+  return (
+    <>
+      <button className="button_borderless_faded">{props.name}</button>
+    </>
+  );
+};
+
+
